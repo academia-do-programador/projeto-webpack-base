3d4281a39bf6f121aedacf2cfb2b14f105b8431e
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-09-04 18:31:17 -0300

    Commit inicial

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,4 +1,6 @@
-import './styles.css';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap";
+
+import "./styles.css";
 
 console.log(uuidv4());
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,73 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-
   // O ponto de entrada da aplicação
   entry: {
-    index: path.resolve(__dirname, 'src/views', 'index.ts')
+    index: path.resolve(__dirname, "src/views", "index.ts"),
   },
 
   // Configuração de output do build
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }      
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 
   // aliases
   resolve: {
-    extensions: ['.ts', '.js', '.css'],
+    extensions: [".ts", ".js", ".css"],
 
     alias: {
-      assets: path.resolve(__dirname, 'src/assets')
-    }
+      assets: path.resolve(__dirname, "src/assets"),
+    },
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/views', 'index.html'),
-      chunks: ['index']
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/views", "index.html"),
+      chunks: ["index"],
     }),
 
     new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/assets', to: 'assets' }
-      ]
-    })
+      patterns: [{ from: "src/assets", to: "assets" }],
+    }),
   ],
 
   // Ambiente de desenvolvimento
-  devtool: 'source-map',
-  
+  devtool: "source-map",
+
   devServer: {
     liveReload: true,
     port: 8080,
     static: {
-      directory: path.resolve(__dirname, 'dist')
+      directory: path.resolve(__dirname, "dist"),
     },
     watchFiles: {
-      paths: ['src']
-    }
-  }
+      paths: ["src"],
+    },
+  },
+
+  // Otimização
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 };
